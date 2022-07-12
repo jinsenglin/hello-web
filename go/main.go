26a66148892be042119fa354e28b34d644871895
Chun Cheng Lin <jinsenglin@users.noreply.github.com>
2022-07-12 18:08:57 +0800

    Add files via upload

--- go/main.go
+++ go/main.go
@@ -6,8 +6,30 @@ import (
     "net/http"
 )
 
+func setCookie(w http.ResponseWriter, r *http.Request) {
+    log.Println("/cookie/set")
+	c := http.Cookie{
+		Name:     "username",
+		Value:    "bater",
+		HttpOnly: true,
+	}
+	http.SetCookie(w, &c)
+}
+
+func getCookie(w http.ResponseWriter, r *http.Request) {
+	c, err := r.Cookie("username")
+	if err != nil {
+		fmt.Fprintln(w, "Cannot get cookie")
+	}
+
+	fmt.Fprintln(w, c)
+}
+
 func main() {
+    http.HandleFunc("/cookie/set", setCookie)
+    http.HandleFunc("/cookie/get", getCookie)
     http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+        log.Println("/")
         fmt.Fprintf(w, "Hello go!")
     })
 
